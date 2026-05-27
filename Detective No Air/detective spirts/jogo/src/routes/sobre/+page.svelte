f1962aae72014f489ccbbf10f37e93380a387168
Max <jmfn2@discente.ifpe.edu.br>
2026-05-27 16:19:01 -0300

    prima commit

---
+++ Detective No Air/detective spirts/jogo/src/routes/sobre/+page.svelte
@@ -0,0 +1,12 @@
+<h1>configurações</h1>
+
+<img class="sobre" src="/images/logoIFPE.png" alt="logo IPFE"/>
+
+<p>
+    Autor: João, Matheus, Gabriel, Lucas
+</p>
+<p>
+  <a href="allan.lima@igarassu.ifpe.edu.br">allan.lima@igarassu.ifpe.edu.br</a>
+</p>
+
+<a class="menu" href="/">Voltar ao Menu</a>
