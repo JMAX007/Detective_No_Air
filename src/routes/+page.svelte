a2cacb8c939394e928be80e346adb9b405022248
Max <jmfn2@discente.ifpe.edu.br>
2026-05-28 19:36:59 -0300

    menu

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    let tituloDoJogo : string = "Detective no Air"
+</script>
+
+<h1>
+    {tituloDoJogo}
+</h1>
+
+<br />
+
+<!-- From Uiverse.io by JohnnyCSilva --> 
+<button class="btn">Button 
+  <span class="icon-right"></span>
+  <span class="icon-right after"></span>
+</button>
+<a class="menu" href="/sobre">Sobre</a>
---
+++ src/routes/sobre/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Configurações</h1>
+
+<img class="sobre" src="/images/logoIFPE.png" alt="logo IPFE"/>
+
+<p>
+    Autor: João, Matheus, Gabriel, Lucas
+</p>
+
+<a class="menu" href="/">Voltar ao Menu</a>
